140f0e92dd69c390fd1307a7eacf577c3f8c96a2
newgames777 <ira766214@gmail.com>
2024-04-15 16:12:35 +0100

    Admin template setup : Add Dashboard

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\View\View;
+
+class DashboardController extends Controller
+{
+    function index(): View {
+        return view('admin.dashboard.index');
+    }
+}
